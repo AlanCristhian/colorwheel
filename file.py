e4f69177a919df9f9757f2ff04b7882b2a8b6532
AlanCristhian <alan.cristh@gmail.com>
2017-03-08 10:53:46 -0300

    improve layout

--- file.py
+++ file.py
@@ -9,7 +9,7 @@ GRID_DIMENSIONS = {
     "padx": (15, 15),
 }
 ENTRY_DIMENSIONS = {
-    "padx": (15, 0),
+    "padx": 0,
     "ipady": 3,
     "ipadx": 6,
 }
@@ -41,37 +41,47 @@ RADIOBUTTON_GRID = {
     "ipady": 3,
     "pady": (15, 7)
 }
-
+VOID = [""]
 
 def luma(R, G, B):
     return 0.299*R + 0.587*G + 0.114*B
 
 
 class File(tkinter.Frame):
-    def __init__(self, root, height=650, width=650):
+    def __init__(self, root, ):
         super().__init__(root)
         self.root = root
+        self.default_sizes = True
+        self.file_path = ""
 
-        self.height = height
-        self.width = width
-        self.position = (20, 20, width - 20, height - 20)
+        self.height = 630
+        self.width = 750
+        self.set_position()
 
         self.create_widgets()
         self.distribute_widgets()
         self.draw_wheel()
         self.set_events()
 
+    def set_position(self):
+        diameter = min(self.width, self.height) - 40
+        x1 = (self.width - diameter)//2
+        y1 = (self.height - diameter)//2
+        x2 = self.width - x1
+        y2 = self.height - y1
+        self.position = (x1, y1, x2, y2)
+
     def create_widgets(self):
         # canvas
         # ======
-        self.canvas = tkinter.Canvas(self, width=650, height=650)
+        self.canvas = tkinter.Canvas(self, width=self.width, height=self.height)
 
         # ========
         # settings
         # ========
 
-        self.settings_frame = tkinter.LabelFrame(self,
-                                                 text="Ajustes de la rueda")
+        self.settings_frame = tkinter.LabelFrame(
+            master=self, text="Ajustes de la rueda")
 
         # number
         # ======
@@ -123,7 +133,7 @@ class File(tkinter.Frame):
 
         # background
         # ==========
-        self.background_var = tkinter.StringVar(self, "#888888")
+        self.background_var = tkinter.StringVar(self, "gray20")
         self.background_label = tkinter.Label(
             self.view_frame, text="Color de fondo:")
         self.background_entry = tkinter.Entry(
@@ -151,23 +161,23 @@ class File(tkinter.Frame):
         self.lchab_radiobutton = tkinter.Radiobutton(
             self.color_space_frame, text="Lab", value="Lab",
             variable=self.color_space_var, command=self.draw_wheel,
-            indicatoron=0)
+            indicatoron=0, highlightthickness=0, padx=6)
         self.lchuv_radiobutton = tkinter.Radiobutton(
             self.color_space_frame, text="Luv", value="Luv",
             variable=self.color_space_var, command=self.draw_wheel,
-            indicatoron=0)
+            indicatoron=0, highlightthickness=0, padx=6)
         self.hsl_radiobutton = tkinter.Radiobutton(
             self.color_space_frame, text="HSL", value="HSL",
             variable=self.color_space_var, command=self.draw_wheel,
-            indicatoron=0)
+            indicatoron=0, highlightthickness=0, padx=6)
         self.hsv_radiobutton = tkinter.Radiobutton(
             self.color_space_frame, text="HSV", value="HSV",
             variable=self.color_space_var, command=self.draw_wheel,
-            indicatoron=0)
+            indicatoron=0, highlightthickness=0, padx=6)
         self.ipt_radiobutton = tkinter.Radiobutton(
             self.color_space_frame, text="IPT", value="IPT",
             variable=self.color_space_var, command=self.draw_wheel,
-            indicatoron=0)
+            indicatoron=0, highlightthickness=0, padx=6)
 
         # ==========
         # data frame
@@ -224,11 +234,9 @@ class File(tkinter.Frame):
                                       selectmode="none")
         self.hexrgb_treeview.configure(yscrollcommand=self.set_scroll)
         self.hexrgba_treeview.configure(yscrollcommand=self.set_scroll)
-
         self.r_treeview.configure(yscrollcommand=self.set_scroll)
         self.g_treeview.configure(yscrollcommand=self.set_scroll)
         self.b_treeview.configure(yscrollcommand=self.set_scroll)
-
 
     def distribute_widgets(self):
         self.grid(padx=0, pady=0)
@@ -302,13 +310,13 @@ class File(tkinter.Frame):
 
         # data treeview
         # =============
-        self.color_treeview.grid(row=0, column=0)
-        self.hexrgb_treeview.grid(row=0, column=1)
-        self.hexrgba_treeview.grid(row=0, column=2)
+        self.color_treeview.grid(row=0, column=0, sticky=tkinter.N+tkinter.S)
+        self.hexrgb_treeview.grid(row=0, column=1, sticky=tkinter.N+tkinter.S)
+        self.hexrgba_treeview.grid(row=0, column=2, sticky=tkinter.N+tkinter.S)
 
-        self.r_treeview.grid(row=0, column=3)
-        self.g_treeview.grid(row=0, column=4)
-        self.b_treeview.grid(row=0, column=5)
+        self.r_treeview.grid(row=0, column=3, sticky=tkinter.N+tkinter.S)
+        self.g_treeview.grid(row=0, column=4, sticky=tkinter.N+tkinter.S)
+        self.b_treeview.grid(row=0, column=5, sticky=tkinter.N+tkinter.S)
 
         self.data_scroll.grid(row=0, column=6, sticky=tkinter.N+tkinter.S)
 
@@ -325,9 +333,11 @@ class File(tkinter.Frame):
         start = self.start_var.get()
         step = 360/number
         background = self.background_var.get()
-        self.canvas.create_rectangle(0, 0, self.width, self.height, fill=background, outline=background)
+        self.canvas.create_rectangle(0, 0, self.width, self.height,
+                                     fill=background, outline=background)
         name = self.color_space_var.get()
         colors = color.space[name](start, number, saturation, luminosity)
+        outline = bool(self.outline_var.get())
 
         tree1 = self.color_treeview.get_children()
         tree2 = self.hexrgb_treeview.get_children()
@@ -335,6 +345,15 @@ class File(tkinter.Frame):
         tree4 = self.r_treeview.get_children()
         tree5 = self.g_treeview.get_children()
         tree6 = self.b_treeview.get_children()
+
+        self_canvas_create_arc = self.canvas.create_arc
+        self_color_treeview_insert = self.color_treeview.insert
+        self_color_treeview_tag_configure = self.color_treeview.tag_configure
+        self_hexrgb_treeview_insert = self.hexrgb_treeview.insert
+        self_hexrgba_treeview_insert = self.hexrgba_treeview.insert
+        self_r_treeview_insert = self.r_treeview.insert
+        self_g_treeview_insert = self.g_treeview.insert
+        self_b_treeview_insert = self.b_treeview.insert
 
         for t1, t2, t3, t4, t5, t6 \
         in zip(tree1, tree2, tree3, tree4, tree5, tree6):
@@ -346,21 +365,23 @@ class File(tkinter.Frame):
             self.b_treeview.delete(t6)
 
         for i, (hex_val, R, G, B) in enumerate(colors):
-            self.canvas.create_arc(self.position, fill=hex_val,
+            self_canvas_create_arc(self.position, fill=hex_val,
                                    start=(i*step), extent=step,
-                                   outline=hex_val)
+                                   outline="black" if outline else hex_val)
 
-            self.color_treeview.insert('', 'end', values=[""], tags=(hex_val,))
-            self.color_treeview.tag_configure(hex_val, background=hex_val)
+            self_color_treeview_insert("", "end", values=VOID, tags=(hex_val,))
+            self_color_treeview_tag_configure(hex_val, background=hex_val)
 
-            self.hexrgb_treeview.insert('', 'end', values=[hex_val])
-            self.hexrgba_treeview.insert('', 'end', values=[hex_val + "ff"])
-            self.r_treeview.insert('', 'end', values=[R])
-            self.g_treeview.insert('', 'end', values=[G])
-            self.b_treeview.insert('', 'end', values=[B])
+            self_hexrgb_treeview_insert("", "end", values=[hex_val])
+            self_hexrgba_treeview_insert("", "end", values=[hex_val + "ff"])
+            self_r_treeview_insert("", "end", values=[R])
+            self_g_treeview_insert("", "end", values=[G])
+            self_b_treeview_insert("", "end", values=[B])
 
-        self.canvas.create_oval(70, 70, self.width - 70, self.height - 70,
-                                fill=background, outline=background)
+        x1, y1, x2, y2 = self.position
+        self.canvas.create_oval(x1 + 50, y1 + 50, x2 - 50, y2 - 50,
+                                fill=background,
+                                outline="black" if outline else background)
         self.canvas.update()
         self.select_changed_value()
 
@@ -389,8 +410,36 @@ class File(tkinter.Frame):
             self.root.clipboard_append(value)
         return copy_content
 
+    def resize_canvas(self, event):
+        if self.default_sizes:
+            settings_w = self.settings_frame.winfo_width()
+            root_w = self.root.winfo_width()
+            root_h = self.root.winfo_height()
+
+            self.width = root_w - settings_w
+            self.height = root_h
+            self.default_sizes = False
+        else:
+            self.width = 650
+            self.height = 650
+            self.default_sizes = True
+
+        # self.root.unbind("<Configure>")
+
+        self.canvas.destroy()
+        self.settings_frame.destroy()
+        self.view_frame.destroy()
+        self.color_space_frame.destroy()
+        self.data_frame.destroy()
+
+        self.set_position()
+        self.create_widgets()
+        self.distribute_widgets()
+        self.draw_wheel()
+        self.set_events()
 
     def set_events(self):
+        # self.root.bind("<Configure>", self.resize_canvas)
         self.number_entry.bind("<Return>", self.draw_wheel)
         self.number_entry.bind("<KP_Enter>", self.draw_wheel)
         self.number_scale.bind("<ButtonRelease-1>", self.draw_wheel)
@@ -410,25 +459,22 @@ class File(tkinter.Frame):
         self.background_entry.bind("<Return>", self.draw_wheel)
         self.background_entry.bind("<KP_Enter>", self.draw_wheel)
 
-        self.color_treeview.bind(
-            "<Double-Button-1>",
-            self.copy_data(self.color_treeview))
-        self.hexrgb_treeview.bind(
-            "<Double-Button-1>",
-            self.copy_data(self.hexrgb_treeview))
-        self.hexrgba_treeview.bind(
-            "<Double-Button-1>",
-            self.copy_data(self.hexrgba_treeview))
-        self.r_treeview.bind(
-            "<Double-Button-1>",
-            self.copy_data(self.r_treeview))
-        self.g_treeview.bind(
-            "<Double-Button-1>",
-            self.copy_data(self.g_treeview))
-        self.b_treeview.bind(
-            "<Double-Button-1>",
-            self.copy_data(self.b_treeview))
+        copy_color_data = self.copy_data(self.color_treeview)
+        copy_hexrgb_data = self.copy_data(self.hexrgb_treeview)
+        copy_hexrgba_data = self.copy_data(self.hexrgba_treeview)
+        copy_r_data = self.copy_data(self.r_treeview)
+        copy_g_data = self.copy_data(self.g_treeview)
+        copy_b_data = self.copy_data(self.b_treeview)
+
+        self.color_treeview.bind("<Double-Button-1>", copy_color_data)
+        self.hexrgb_treeview.bind("<Double-Button-1>", copy_hexrgb_data)
+        self.hexrgba_treeview.bind("<Double-Button-1>", copy_hexrgba_data)
+        self.r_treeview.bind("<Double-Button-1>", copy_r_data)
+        self.g_treeview.bind("<Double-Button-1>", copy_g_data)
+        self.b_treeview.bind("<Double-Button-1>", copy_b_data)
 
 
 if __name__ == '__main__':
-    File(tkinter.Tk()).root.mainloop()
+    root = tkinter.Tk()
+    file = File(root)
+    root.mainloop()
