e0bf622476b394017c32ab6bcc25033e2c8ebaf0
AlanCristhian <alan.cristh@gmail.com>
2017-03-15 21:03:57 -0300

    add JCh color spacious and mixer

--- color.py
+++ color.py
@@ -1,7 +1,16 @@
+from math import radians, cos, sin
+
 from colormath.color_objects import (LCHuvColor, LCHabColor, HSLColor,
                                      HSVColor, IPTColor, sRGBColor)
 from colormath.color_conversions import convert_color
-from math import radians, cos, sin
+from colorspacious import cspace_convert
+
+
+def to_hex_rgb(gen):
+    ans = ((v.clamped_rgb_r, v.clamped_rgb_g,v.clamped_rgb_b) for v in gen)
+    ans = ((round(r*255), round(g*255), round(b*255)) for r, g, b in ans)
+    ans = (("#%02x%02x%02x" % (r, g, b), r, g, b) for r, g, b in ans)
+    return ans
 
 
 def lchab(l, c, h):
@@ -29,14 +38,9 @@ def ipt(l, c, h):
 
 def create_system(space):
     def hex_colors(start, amount, saturation, luminosity):
-        c = saturation
-        l = luminosity
         k = 360/amount
-        ans = (space(l, c, start + i*k) for i in range(amount))
-        ans = ((v.clamped_rgb_r, v.clamped_rgb_g,v.clamped_rgb_b) for v in ans)
-        ans = ((round(r*255), round(g*255), round(b*255)) for r, g, b in ans)
-        ans = (("#%02x%02x%02x" % (r, g, b), r, g, b) for r, g, b in ans)
-        return ans
+        ans = (space(luminosity, saturation, start+i*k) for i in range(amount))
+        return to_hex_rgb(ans)
     return hex_colors
 
 
@@ -65,12 +69,33 @@ def create_mixer(space):
     return mix
 
 
+def jch(start, amount, saturation, luminosity):
+    k = 360/amount
+    ans = ((luminosity, saturation, start + i*k) for i in range(amount))
+    ans = (cspace_convert(color, "JCh", "sRGB1") for color in ans)
+    ans = (color.tolist() for color in ans)
+    ans = (sRGBColor(*color) for color in ans)
+    return to_hex_rgb(ans)
+
+
+def jch_mixer(color1, color2):
+        rgb1, rgb2 = hex_to_rgb(color1), hex_to_rgb(color2)
+        x1, y1, z1 = cspace_convert(rgb1, "sRGB255", "JCh").tolist()
+        x2, y2, z2 = cspace_convert(rgb2, "sRGB255", "JCh").tolist()
+        ans = ((x1 + x2)/2, (y1 + y2)/2 , (z1 + z2)/2)
+        ans = cspace_convert(ans, "JCh", "sRGB1").tolist()
+        ans = sRGBColor(*ans)
+        ans = (ans.clamped_rgb_r, ans.clamped_rgb_g, ans.clamped_rgb_b)
+        ans = tuple(round(i*255) for i in ans)
+        return "#%02x%02x%02x" % ans
+
 space = {
     "Lab": create_system(lchab),
     "Luv": create_system(lchuv),
     "HSL": create_system(hsl),
     "HSV": create_system(hsv),
     "IPT": create_system(ipt),
+    "JCh": jch,
 }
 
 
@@ -80,4 +105,5 @@ mixer = {
     "HSL": create_mixer(HSLColor),
     "HSV": create_mixer(HSLColor),
     "IPT": create_mixer(IPTColor),
+    "JCh": jch_mixer,
 }
